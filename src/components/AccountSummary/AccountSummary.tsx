74b03b08915fc9cbaba43c443381655fcfa763b3
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-10 14:00:32 +0500

    Added Social Icons and Improved Accessibility

--- src/components/AccountSummary/AccountSummary.tsx
+++ src/components/AccountSummary/AccountSummary.tsx
@@ -17,7 +17,7 @@ const AccountSummary: React.FC = () => {
                     <img className={styles.image} src="/images/balance.svg" alt="Current Balance"/>
                 </div>
                 <h2><CountUp start={previousBalance} end={currentBalance} duration={2} /></h2>
-                <h4>Current Balance</h4>
+                <p>Current Balance</p>
             </div>
 
             <div className={styles.income}>
@@ -25,7 +25,7 @@ const AccountSummary: React.FC = () => {
                     <img className={styles.image} src="/images/income.svg" alt="Total Income"/>
                 </div>
                 <h2><CountUp start={previousIncome} end={totalIncome} duration={2} /></h2>
-                <h4>Total Income</h4>
+                <p>Total Income</p>
             </div>
 
             <div className={styles.expense}>
@@ -33,7 +33,7 @@ const AccountSummary: React.FC = () => {
                     <img className={styles.image} src="/images/expense.svg" alt="Total Expense"/>
                 </div>
                 <h2><CountUp start={previousExpense} end={totalExpense} duration={2} /></h2>
-                <h4>Total Expenditure</h4>
+                <p>Total Expenditure</p>
             </div>
 
         </div>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+// Icons
+import { IconContext } from 'react-icons';
+import { FaGithub, FaFacebookSquare } from 'react-icons/fa';
 // Styles
 import styles from './Footer.module.css';
 
@@ -9,14 +12,24 @@ const Footer = () => {
             <h2>My Expense Tracker</h2>
             <p>Made By</p>
             <h3>Mian Muhammad Sharjeel Safdar</h3>
-            <h4>
-                <a  href="https://www.google.com" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                >
-                    Git Repository Link
-                </a>
-            </h4>
+            <IconContext.Provider value={{ color: "white", size: "2rem", }}>
+                <div className={styles.socialIcons}>
+                <a 
+                        href="https://github.com/SharjeelSafdar/project7a1-expense-tracker-pwa" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <FaGithub className={styles.socialIcon} title="Contact Us" />
+                    </a>
+                    <a 
+                        href="https://web.facebook.com/mian.safdar.3386" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <FaFacebookSquare className={styles.socialIcon} title="Contact Us" />
+                    </a>
+                </div>
+            </IconContext.Provider>
         </div>
     );
 }
