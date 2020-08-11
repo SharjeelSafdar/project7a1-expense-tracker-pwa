58ae7806df6e3768d7e34842191faa207a7fad33
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-11 13:56:46 +0500

    Updated firebase app id

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,5 +8,5 @@ firebase.initializeApp({
     projectId: "expense-tracker-project7-3ff82",
     storageBucket: "expense-tracker-project7-3ff82.appspot.com",
     messagingSenderId: "164630845172",
-    appId: "1:164630845172:web:28b66170ae54066426b0a1"
+    appId: "1:164630845172:web:ef83173cfccff9e926b0a1"
 })
--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ var firebaseConfig = {
     projectId: "expense-tracker-project7-3ff82",
     storageBucket: "expense-tracker-project7-3ff82.appspot.com",
     messagingSenderId: "164630845172",
-    appId: "1:164630845172:web:28b66170ae54066426b0a1"
+    appId: "1:164630845172:web:ef83173cfccff9e926b0a1"
 };
 
 // Initialize Firebase
