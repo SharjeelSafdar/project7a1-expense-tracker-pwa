04108189e6016e9d67e88c6035fb5caf7f570d4b
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-11 13:38:07 +0500

    Separate SW for firebase

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/7.17.2/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/7.17.2/firebase-messaging.js')
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAIHHrZB-JRqq0XVCp8NicXCE_wZ1CW8Qw",
+    authDomain: "expense-tracker-project7-3ff82.firebaseapp.com",
+    databaseURL: "https://expense-tracker-project7-3ff82.firebaseio.com",
+    projectId: "expense-tracker-project7-3ff82",
+    storageBucket: "expense-tracker-project7-3ff82.appspot.com",
+    messagingSenderId: "164630845172",
+    appId: "1:164630845172:web:28b66170ae54066426b0a1"
+})
